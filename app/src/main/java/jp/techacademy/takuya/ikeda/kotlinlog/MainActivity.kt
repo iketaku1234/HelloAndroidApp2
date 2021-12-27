4ab69c452ec65621793519e4b80b6fdb85e7bbd0
Iketaku1234 <jpu122124@gmail.com>
2021-12-27 23:54:04 +0900

    L3の課題本提出

--- app/src/main/java/jp/techacademy/takuya/ikeda/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takuya/ikeda/kotlinlog/MainActivity.kt
@@ -9,8 +9,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val human = Human("太郎", 20,"野球") // 名前を太郎、年齢20歳で、Humanのインスタンスを作る
+        val human = Human("太郎", 20,"野球") // 名前を太郎、年齢20歳で、趣味が野球のHumanのインスタンスを作る
+        val human2 = Human("花子", 25,"手芸")//名前を花子、年齢25歳で、趣味が手芸のHumanのインスタンスを作る
+
         human.say()
-        human.think()
+        human2.think()
     }
 }
